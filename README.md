c112650866f8647edf8afd82101404ed2a0526cb
Anthony Lubrino <anthonylubrino@eltorolaptop83.eltoro.com>
2019-10-31 16:11:12 -0400

    adding exisiting code

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Json-to-fixture
-converts JSON objects into data fixture files
+
+  This script will take any .json file and convert it into a javascript object
+  and then add it to a fixture file.
+  by default, it will append all .json files in the "JSONs" directory 
+  to the Campaign Fixture.
+  5 arguments can be passed into this script
+
+  if anything is passed in the first argument, it will replace the current fixture with new data, if an empty string is passed,
+  it will append the new data to the end of the existing fixture file.
+
+  The second argument will set a specific fixture collection to add the json file to.  If empty string, it will default to 'Campaigns'.
+
+  The third will add a specific .json file. If empty string, it will default to all files in the "jsons" directory inside fixtures.
+
+  The fourth will set a specific name for the object after it has been added to the fixture. If empty string, it 
+  will default to the name of the campaign with a random hash at the end  i.e. "campaign_name_UI3748D0"
+
+  The fifth will set a relative base directory to pull json file from. If empty string, it will default to '../src/requests/fixtures/jsons'
+
+  example with arguments: "node addFixture reset Creatives test.json fixture_4 ../src/fixtures/myJSONfolder"
---
+++ jsonToFixture.js
@@ -0,0 +1,52 @@
+
+
+const fs = require("fs")
+const path = require("path")
+const JSONPath = process.argv[6] || `../src/requests/fixtures/jsons`
+const basePath = path.join(__dirname, JSONPath)
+const passedItem = process.argv[4] || null
+const collection = process.argv[3] || 'Campaigns'
+const fixtureName = `${collection}.ts`
+const fixturePath = path.join(__dirname, '..', 'src', 'requests', 'fixtures')
+const randomGen = () => {
+  return Math.random().toString(36).replace('0.', '') 
+}
+const newFixtureName = `${collection}.ts`
+const initData =`export const ${collection} = {}`
+const appendFixture = (item, fixture) => {
+  fs.readFile(path.join(basePath, '/', item ), 'utf8', (err,data) => {
+    if (err) throw err;
+    const JSData = data.replace(/"([^"]+)":/g, '$1:')
+    const dataObj = JSON.parse(data)
+    const fieldName = process.argv[5] || `${dataObj.name.replace(/\s/g, "_")}_${randomGen()}`
+    const writeData =`\n${collection}.${fieldName} = ${JSData}\n`
+    fs.appendFile(path.join(fixturePath, '/', fixture), writeData, (err) => {
+      if (err) throw err;
+      console.log(`updated ${fixtureName} with new fixture data: "${fieldName}".`);
+    })
+  })
+}
+fs.readdir(fixturePath, (err, items) => {
+  const existingFixtures = items.filter(item => item === collection || item === fixtureName)
+  if(existingFixtures.length === 0){
+    fs.writeFile(path.join(fixturePath, '/', newFixtureName), initData, err => {
+      if (err) throw err;
+      console.log(`new fixture file ${newFixtureName} created`);
+      if(passedItem) appendFixture(passedItem, newFixtureName)
+      fs.readdir(basePath, (err, items) => items.map(item => appendFixture(item, newFixtureName)))
+    });
+  } else if(process.argv[2]) {
+    fs.truncate(path.join(fixturePath, '/', fixtureName), 28, err => {
+      if (err) throw err;
+      console.log(`removed existing fixture data from ${fixtureName}`);
+      if(passedItem) appendFixture(passedItem, fixtureName)
+      fs.readdir(basePath, (err, items) => items.map(item => appendFixture(item, fixtureName)))
+    });
+  } else {
+    if(passedItem) appendFixture(passedItem, fixtureName)
+    fs.readdir(basePath, (err, items) => items.map(item => appendFixture(item, fixtureName)))  
+  } 
+})
+
+
+
